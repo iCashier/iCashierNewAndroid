073e4b54c991635f9391d3b2d56f4841aa993c64
Shashank <Shashank@!#~12>
2021-03-08 15:51:38 +0530

    new21

--- app/src/main/java/com/icashier/app/dialog/OrderDetailDialog.java
+++ app/src/main/java/com/icashier/app/dialog/OrderDetailDialog.java
@@ -87,7 +87,6 @@ public class OrderDetailDialog extends Dialog implements ReceiveListener {
     private List<OrderListResponse.ResultBean.ItemsBean> itemList = new ArrayList<>();
     private SupportMapFragment mapFragment;
     OrderItemsAdapter adapter;
-    String afdsaf;
 
     public OrderDetailDialog(HomeActivity context, OrderListResponse.ResultBean orderData) {
         super(context);
